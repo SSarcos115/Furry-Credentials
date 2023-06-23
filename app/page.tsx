95ff9e34c41157ebe2de71872016e39db7df2494
Sebastian <bastians12@gmail.com>
2023-06-23 00:05:20 -0400

    just trying stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <h1 className="text-green-500">Hello World</h1>;
+  return (
+    <div>
+      <h1 className="text-green-500">Hello World</h1>
+      <h2>Hello World Again</h2>
+    </div>
+  );
 }
 
 export default Home;
